ae099982b7db1dcaa992c32e41644e5d28b1e3e9
Tim Stirrat <tstirrat@google.com>
2015-06-09 10:34:49 -0700

    Update dependency compat to require beta.18+
    All previous versions will not work. This is part 1 of cleanup in preparation for 1.0 final

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,48 +1,21 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-data-beta.12',
-      dependencies: {
-        'ember-data': '1.0.0-beta.12'
-      },
-      resolutions: {
-        'ember-data': '1.0.0-beta.12'
-      }
-    },
-    {
-      name: 'ember-data-beta.14.1',
-      dependencies: {
-        'ember-data': '1.0.0-beta.14.1'
-      },
-      resolutions: {
-        'ember-data': '1.0.0-beta.14.1'
-      }
-    },
-    {
-      name: 'ember-data-beta.16.1',
-      dependencies: {
-        'ember-data': '1.0.0-beta.16.1'
-      },
-      resolutions: {
-        'ember-data': '1.0.0-beta.16.1'
-      }
-    },
-    {
-      name: 'ember-data-beta.17',
-      dependencies: {
-        'ember-data': '1.0.0-beta.17'
-      },
-      resolutions: {
-        'ember-data': '1.0.0-beta.17'
-      }
-    },
-    {
       name: 'ember-data-beta.18',
       dependencies: {
         'ember-data': '1.0.0-beta.18'
       },
       resolutions: {
         'ember-data': '1.0.0-beta.18'
+      }
+    },
+    {
+      name: 'ember-data-beta.19',
+      dependencies: {
+        'ember-data': '1.0.0-beta.19'
+      },
+      resolutions: {
+        'ember-data': '1.0.0-beta.19'
       }
     },
     {
